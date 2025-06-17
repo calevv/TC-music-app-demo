dd73c1ab9258e22a79f4c4c73f7d12d58d46009b
calevv <ika818488@gmail.com>
2025-06-17 11:38:22 +0900

    feat:서치페이지 음악추가 및 전체 레이아웃 세로 스크롤 수정

--- src/stores/usePlaylistStore.ts
+++ src/stores/usePlaylistStore.ts
@@ -1,13 +1,13 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 interface PlaylistState {
-    playlistId: string | null;
-    setPlaylistId: (id: string | null) => void;
-    clearPlaylistId: () => void;
+  playlistId: string | null;
+  setPlaylistId: (id: string | null) => void;
+  clearPlaylistId: () => void;
 }
 
 export const usePlaylistStore = create<PlaylistState>((set) => ({
-    playlistId: null,
-    setPlaylistId: (id) => set({ playlistId: id }),
-    clearPlaylistId: () => set({ playlistId: null }),
+  playlistId: null,
+  setPlaylistId: (id) => set({ playlistId: id }),
+  clearPlaylistId: () => set({ playlistId: null }),
 }));
